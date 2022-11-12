56fecfd99ee6bdd63ada651bb01a3af87d948a51
MankaranSingh <mankaran32@gmail.com>
2022-11-12 21:48:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setup(name="flowpilot",
                                         "logmessaged=selfdrive.logmessaged:main",
                                         "keyvald=selfdrive.keyvald:main",
                                         "pandad=selfdrive.boardd.pandad:run",
-                                        "flowinit=selfdrive.flowinit.flowinitd:main"]}
+                                        "flowinit=selfdrive.manager.flowinitd:main"]}
      )
 
