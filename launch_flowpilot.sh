e78425de419c5e7927e46d1a49dcb2fd76dde4d7
MankaranSingh <mankaran32@gmail.com>
2022-11-25 20:11:02 +0530

    add support for IP CAN

--- launch_flowpilot.sh
+++ launch_flowpilot.sh
@@ -9,8 +9,8 @@ export USE_GPU="1" # no affect on android, gpu always used on android
 export PASSIVE="0"
 #export MSGQ="1"
 #export USE_PARAMS_NATIVE="1"
+#DISCOVERABLE_PUBLISHERS="1"
 export ZMQ_MESSAGING_PROTOCOL="TCP" # TCP, INTER_PROCESS, SHARED_MEMORY
-export ZMQ_MESSAGING_ADDRESS="127.0.0.1"
 
 export SIMULATION="1"
 #export FINGERPRINT="HONDA CIVIC 2016"
