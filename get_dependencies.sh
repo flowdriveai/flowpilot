2ed59f710367b139795cee44725d56cd425f2c46
MankaranSingh <mankaran32@gmail.com>
2022-11-25 15:30:57 +0530

    fix cython version in mismatch in get_dependencies

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -1,6 +1,6 @@
 # these need to be installed before requirements.txt 
 pip install pkgconfig==1.5.5
-pip install Cython==0.29.30
+pip install Cython==0.29.32
 
 sudo apt-get install -y rsync clang capnproto libcapnp-dev libzmq3-dev cmake libjson11-1 libjson11-1-dev liblmdb-dev libusb-1.0-0-dev
 sudo apt-get install -y dfu-util gcc-arm-none-eabi libcurl4-openssl-dev libssl-dev
