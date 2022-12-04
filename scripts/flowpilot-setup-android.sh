a3271d29d348de31710de55960b7f4227b3b6de2
MankaranSingh <mankaran32@gmail.com>
2022-12-04 16:18:10 +0530

    refactor install scripts

--- scripts/flowpilot-setup-android.sh
+++ scripts/flowpilot-setup-android.sh
@@ -1,3 +1,5 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
 set -e
 
 # White
@@ -33,74 +35,74 @@ add_to_file() {
 yes | termux-setup-storage
 
 # install base packages
-pkg update && yes | pkg upgrade
-pkg install -y proot-distro termux-api net-tools openssh wget
+pkg install -y proot-distro termux-api net-tools openssh wget tsu
 
 # download latest apks
-LATEST_FLOWPILOT_APK=$(curl -s https://api.github.com/repos/flowdriveai/flowpilot/releases/latest | grep browser_download_url | cut -d '"' -f 4)
-wget -q --show-progress --no-clobber --tries=5 $LATEST_FLOWPILOT_APK -P /sdcard/flowpilot/Downloads
+FLOWPILOT_APK_URL=$(curl -s https://api.github.com/repos/flowdriveai/flowpilot/releases/latest | grep browser_download_url | cut -d '"' -f 4)
+wget -q --show-progress --no-clobber --tries=5 $FLOWPILOT_APK_URL -P /sdcard/flowpilot/Downloads
 
-TERMUX_API_APK='https://f-droid.org/repo/com.termux.api_51.apk'
-wget -q --show-progress --no-clobber --tries=5 $TERMUX_API_APK -P /sdcard/flowpilot/Downloads
+TERMUX_API_APK_URL='https://f-droid.org/repo/com.termux.api_51.apk'
+wget -q --show-progress --no-clobber --tries=5 $TERMUX_API_APK_URL -P /sdcard/flowpilot/Downloads
+
+INSTALL_SCRIPT_URL=''
+wget -q --show-progress --no-clobber --tries=5 $INSTALL_SCRIPT_URL -P /sdcard/flowpilot/Download
 
 # setup startup commands
 add_to_file 'sshd' ~/.bashrc
 add_to_file 'termux-wake-lock' ~/.bashrc
 
-function setup_nonroot_env {
-        proot-distro install ubuntu || info "Ubuntu already installed.. skipping"
+setup_nonroot_env() {
+        proot-distro install ubuntu || info "ubuntu already installed.. skipping"
         add_to_file $'alias login=\'proot-distro login ubuntu\'' ~/.bashrc
 
-        proot_exec "echo 'export ANDROID_DATA=\'\'' >> ~/.bashrc"
-        proot_exec "apt update && apt install -y sudo git"
-        proot_exec "DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata"
-        proot_exec "sudo apt install -y software-properties-common"
-        proot_exec "yes | sudo add-apt-repository ppa:deadsnakes/ppa" || true
-        proot_exec "sudo apt install -y python3.9 python3.9-dev python3.9-distutils python-is-python3 python3-pip"
-        proot_exec "sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.9 1"
-        proot_exec "sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1"
-
-        proot_exec "[ ! -d 'flowpilot' ] && git clone https://github.com/flowdriveai/flowpilot.git" || true
-        proot_exec "cd flowpilot && git submodule update --init"
-
-        proot_exec "cd flowpilot && ANDROID_DATA='' ./get_dependencies.sh"
-        proot_exec "cd flowpilot && ANDROID_DATA='' scons"
+        ln -rs $PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu $HOME/flowpilot_env
+        cp /sdcard/flowpilot/Download/install-flowpilot $HOME/flowpilot_env/bin
 }
 
 # TODO: fix this
-function setup_root_env {
+setup_root_env() {
+        
+
         ROOTFS_TAR="ubuntu-focal-core-cloudimg-arm64-root-2020.12.10.tar.gz"
         wget -q --show-progress --no-clobber --tries=5 https://github.com/termux/proot-distro/releases/download/v1.2-ubuntu-focal-rootfs/$ROOTFS_TAR -P /sdcard/flowpilot/Downloads/
-        mkdir -p chroot
-        sudo tar xfp /sdcard/flowpilot/Download/$ROOTFS_TAR -C ./chroot
+        
+        CHROOT=/data/data/com.termux/files/home/flowpilot_env_root
+        mkdir -p $CHROOT
+        sudo tar --skip-old-files xfp /sdcard/flowpilot/Downloads/$ROOTFS_TAR -C $CHROOT
 
-        echo """
-            #!/data/data/com.termux/files/usr/bin/sh
+        cp /sdcard/flowpilot/Download/install-flowpilot $HOME/flowpilot_env_root/bin
+
+        rm /data/data/com.termux/files/usr/bin/login-root
+        echo "
+            #!/data/data/com.termux/files/usr/bin/bash
 
             # prepare mount external storage
-            mkdir -p ./chroot/sdcard
+            mkdir -p $CHROOT/sdcard
 
             # fix /data mount options
             mount -o remount,dev,suid /data
 
-            mount --bind /dev ./chroot/dev
-            mount --bind /sys ./chroot/sys
-            mount --bind /proc ./chroot/proc
-            mount --bind /dev/pts ./chroot/dev/pts
-            mount --bind /sdcard ./chroot/sdcard
+            mount --bind /dev $CHROOT/dev
+            mount --bind /sys $CHROOT/sys
+            mount --bind /proc $CHROOT/proc
+            mount --bind /dev/pts $CHROOT/dev/pts
+            mount --bind /sdcard $CHROOT/sdcard
 
             # disable termux-exec
             unset LD_PRELOAD
 
             export PATH=/bin:/sbin:/usr/bin:/usr/sbin
-            export TERM=$TERM
+            export TERM=\$TERM
             export TMPDIR=/tmp
 
-            chroot ./chroot /bin/su - root
-        """ >> login-root.sh
-
-        chmod +x run-root.sh
+            chroot $CHROOT /bin/su - root
+        " >> /data/data/com.termux/files/usr/bin/login-root
+        chmod +x /data/data/com.termux/files/usr/bin/login-root
 }
 
-
 setup_nonroot_env
+setup_root_env
+
+success "Flowpilot environments installed."
+success "login to non-root env with `login`"
+success "login to root env with `sudo login-root`"
