9093cebf1a9e9a50cb1c6db0d3feb0d48a51d6da
MankaranSingh <mankaran32@gmail.com>
2023-04-22 14:46:34 +0530

    bump cereal, add livestreaming support

--- launch_flowpilot.sh
+++ launch_flowpilot.sh
@@ -10,9 +10,10 @@ export USE_GPU="0" # no affect on android, gpu always used on android
 export PASSIVE="0"
 #export MSGQ="1"
 #export USE_PARAMS_NATIVE="1"
-#export DISCOVERABLE_PUBLISHERS="1"
 export ZMQ_MESSAGING_PROTOCOL="TCP" # TCP, INTER_PROCESS, SHARED_MEMORY
-#export IP_CAN_ADDR="127.0.0.1"
+
+#export DISCOVERABLE_PUBLISHERS="1" # if enabled, other devices on same network can access sup/pub data.
+#export DEVICE_ADDR="127.0.0.1" # connect to external device running flowpilot over same network. useful for livestreaming.
 
 export SIMULATION="1"
 #export FINGERPRINT="HONDA CIVIC 2016"
