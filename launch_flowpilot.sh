dbf3705d9ea5ab5a5d2e754c051257059b1ea5d4
MankaranSingh <mankaran32@gmail.com>
2023-04-29 19:14:23 +0530

    use tmux

--- launch_flowpilot.sh
+++ launch_flowpilot.sh
@@ -1,9 +1,6 @@
 set -e
 source ./.env
 
-# build changes
-scons
-
 export WIDE_ROAD_CAMERA_SOURCE="selfdrive/assets/fcam.avi" # no affect on android
 export ROAD_CAMERA_SOURCE="selfdrive/assets/tmp" # no affect on android
 export USE_GPU="0" # no affect on android, gpu always used on android
@@ -21,4 +18,23 @@ export SIMULATION="1"
 ## android specific ##
 export USE_SNPE="0" # only works for snapdragon devices.
 
-flowinit
+
+if ! command -v tmux &> /dev/null
+then
+    echo "tmux could not be found, installing.."
+    sudo apt-get update
+    sudo apt-get install tmux
+fi
+
+
+if pgrep -x "flowinit" > /dev/null
+    then
+        echo "another instance of flowinit is already running"
+        exit
+    else
+        # start a tmux pane
+        tmux new-session -d -s "flowpilot" "scons && flowinit"
+        tmux attach -t flowpilot
+fi
+
+while true; do sleep 1; done
