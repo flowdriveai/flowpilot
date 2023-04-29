79280224290fead77419dfc276989d9baef11a19
MankaranSingh <mankaran32@gmail.com>
2023-04-29 19:12:47 +0530

    use tmux

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -1,9 +1,11 @@
+sudo apt-get update
+
 # these need to be installed before requirements.txt 
 pip install pkgconfig==1.5.5
 pip install Cython==0.29.32
 
 sudo apt-get install -y rsync clang capnproto libcapnp-dev libzmq3-dev cmake libjson11-1 libjson11-1-dev liblmdb-dev libusb-1.0-0-dev
-sudo apt-get install -y dfu-util gcc-arm-none-eabi libcurl4-openssl-dev libssl-dev ffmpeg libeigen3-dev nano
+sudo apt-get install -y dfu-util gcc-arm-none-eabi libcurl4-openssl-dev libssl-dev ffmpeg libeigen3-dev nano tmux
 
 # install capnpc-java
 if ! command -v capnpc-java --version &> /dev/null  # TODO: Running through scons misses this
