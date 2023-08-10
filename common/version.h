755e6c66407f286a7bc22f433ea89293b5a9cf70
MankaranSingh <mankaran32@gmail.com>
2023-08-10 22:16:53 +0530

    0.2.4

--- common/version.h
+++ common/version.h
@@ -1,2 +1,2 @@
 // hard-forked from https://github.com/commaai/openpilot/tree/05b37552f3a38f914af41f44ccc7c633ad152a15/selfdrive/common/version.h
-#define FLOWPILOT_VERSION "0.2.3"
+#define FLOWPILOT_VERSION "0.2.4"
