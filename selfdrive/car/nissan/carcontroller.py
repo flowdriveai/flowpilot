5ce53e3f115fa746ab0f8390a352092bbbfbb8b0
MankaranSingh <mankaran32@gmail.com>
2023-08-14 11:09:50 +0530

    car interface update

--- selfdrive/car/nissan/carcontroller.py
+++ selfdrive/car/nissan/carcontroller.py
@@ -63,10 +63,11 @@ class CarController:
     can_sends.append(nissancan.create_steering_control(
       self.packer, apply_angle, self.frame, CC.latActive, self.lkas_max_torque))
 
+    # Below are the HUD messages. We copy the stock message and modify
     if self.CP.carFingerprint != CAR.ALTIMA:
       if self.frame % 2 == 0:
-        can_sends.append(nissancan.create_lkas_hud_msg(
-          self.packer, CS.lkas_hud_msg, CC.enabled, hud_control.leftLaneVisible, hud_control.rightLaneVisible, hud_control.leftLaneDepart, hud_control.rightLaneDepart))
+        can_sends.append(nissancan.create_lkas_hud_msg(self.packer, CS.lkas_hud_msg, CC.enabled, hud_control.leftLaneVisible, hud_control.rightLaneVisible,
+                                                       hud_control.leftLaneDepart, hud_control.rightLaneDepart))
 
       if self.frame % 50 == 0:
         can_sends.append(nissancan.create_lkas_hud_info_msg(
